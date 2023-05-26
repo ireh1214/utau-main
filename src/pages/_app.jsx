ae093444b8fc432caf957e4f42805711c23df49a
unknown <ireh1214@gmail.com>
2023-05-26 10:27:13 +0900

    dsafsdfas

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -6,7 +6,7 @@ export default function App({ Component, pageProps }) {
   return (
     <div>
       <Head>
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="google" content="notranslate" />
         <link rel="shortcut icon" href="fav.ico" type="image/x-icon" />
